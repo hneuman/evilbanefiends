434cf7a5f747920e015876969084d015a3644245
hernan neuman <hernanneuman15@hotmail.com>
2016-05-18 18:14:29 -0400

    creando proyecto

--- test_rest/settings.py
+++ test_rest/settings.py
@@ -121,3 +121,4 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, '/apprest/static/')
+COLLECTFAST_CACHE = 'collectfast'
